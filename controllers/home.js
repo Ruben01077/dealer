0053e90b79f8a1fe0b3ea7f97dca9bd93142d9e5
Rub <melikyan740@gmail.com>
2023-02-12 11:12:16 -0800

    adding default view

---
+++ controllers/home.js
@@ -0,0 +1,10 @@
+const router = require("express").Router()
+
+function home(){
+
+
+}
+
+
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express")
 const app = express()
+require("dotenv").config()
+
+
+app.use("/inventory", require("./controllers/inventory"))
+
 
 app.get("/", (req, res) =>{
     res.send("Hello world")
 })
 
 
-app.listen(3000)
+
+app.get("*", (req, res) =>{
+
+    res.status(404).send("Wrong page 404 Error")
+
+})
+
+
+app.listen(process.env.PORT)
